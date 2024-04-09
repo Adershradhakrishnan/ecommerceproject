57b2049cace175c99f0ca094b583b1d8a5506330
adershradhakrishnan <adershradhakrishnan07@gmail.com>
2024-04-09 14:50:26 +0530

    deee message

---
+++ client/my-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import SignupForm from './components/Signup/Signup';
+import SigninForm from './components/Signin/Signin';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <SignupForm/>
+    <SigninForm/>
+    </>
+  )
+}
+
+export default App
